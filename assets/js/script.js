bcd15d1c29ae7b1dbd4d56ad842a008c1dafcea7
oljung <patrik_osterljung@hotmail.com>
2021-08-14 12:15:23 +0000

    feat: add code to increment score and incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,8 +43,10 @@ function checkAnswer(){
 
     if(isCorrect){
         alert('Hey! You got it right! :D');
+        incrementScore();
     } else {
         alert(`Awwww... you answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -63,12 +65,20 @@ function calculateCorrectAnswer(){
     }
 }
 
+/**
+ * Gets the current score from the DOM and then increments it by 1
+ */
 function incrementScore(){
-
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and then increments it by 1
+ */
 function incrementWrongAnswer(){
-
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2){
